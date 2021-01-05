bbeb34987204f3718afc1f5fdc2912a827776560
Sreelekha <sreesrini72@gmail.com>
2021-01-05 22:03:35 +0530

    Updates

---
+++ precache-manifest.6305119aa600aaaab1dcc9f296dcf879.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97fa316b511bef352d91aaa71d334ba2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a91208ef739eb601146e",
+    "url": "/portfolio/static/css/14.ef440c72.chunk.css"
+  },
+  {
+    "revision": "f91616ce0cd673bcb350",
+    "url": "/portfolio/static/css/main.6da6e251.chunk.css"
+  },
+  {
+    "revision": "a91208ef739eb601146e",
+    "url": "/portfolio/static/js/14.8faaab3d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/portfolio/static/js/14.8faaab3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f91616ce0cd673bcb350",
+    "url": "/portfolio/static/js/main.e91ca4ac.chunk.js"
+  },
+  {
+    "revision": "29ffa131420469805684",
+    "url": "/portfolio/static/js/reactPlayerDailyMotion.c3b52d32.chunk.js"
+  },
+  {
+    "revision": "b2203a9fb4b30a81304d",
+    "url": "/portfolio/static/js/reactPlayerFacebook.63f91871.chunk.js"
+  },
+  {
+    "revision": "f1f440219bfa45e3adb6",
+    "url": "/portfolio/static/js/reactPlayerFilePlayer.de772b44.chunk.js"
+  },
+  {
+    "revision": "50a78f3b565544199a2c",
+    "url": "/portfolio/static/js/reactPlayerMixcloud.910d21a5.chunk.js"
+  },
+  {
+    "revision": "baeedce804d631d64bb8",
+    "url": "/portfolio/static/js/reactPlayerPreview.745def88.chunk.js"
+  },
+  {
+    "revision": "a6237d371986dcc00519",
+    "url": "/portfolio/static/js/reactPlayerSoundCloud.f933b253.chunk.js"
+  },
+  {
+    "revision": "d817bf099f2a7569c803",
+    "url": "/portfolio/static/js/reactPlayerStreamable.3ee872cc.chunk.js"
+  },
+  {
+    "revision": "758338d91ef54e8f9883",
+    "url": "/portfolio/static/js/reactPlayerTwitch.b7ede118.chunk.js"
+  },
+  {
+    "revision": "cbfec4ddb382e13f7c5c",
+    "url": "/portfolio/static/js/reactPlayerVidyard.b448d65a.chunk.js"
+  },
+  {
+    "revision": "67fd9ae4bef8c42f70c3",
+    "url": "/portfolio/static/js/reactPlayerVimeo.7ad53ffe.chunk.js"
+  },
+  {
+    "revision": "d5449e491ebb486e2d96",
+    "url": "/portfolio/static/js/reactPlayerWistia.d0887a77.chunk.js"
+  },
+  {
+    "revision": "cd5b56e0137b5e929e55",
+    "url": "/portfolio/static/js/reactPlayerYouTube.7ef97007.chunk.js"
+  },
+  {
+    "revision": "5c2d9ba61f399781164f",
+    "url": "/portfolio/static/js/runtime-main.81d33a07.js"
+  },
+  {
+    "revision": "c46d7dfcd32825e323952f4d13c6f35c",
+    "url": "/portfolio/static/media/digitalartist.c46d7dfc.jpg"
+  },
+  {
+    "revision": "a2d7e7ba49800d00803f4b8428596da7",
+    "url": "/portfolio/static/media/instagram.a2d7e7ba.svg"
+  },
+  {
+    "revision": "ab36c3a0983a76a12fd8d16233a0e965",
+    "url": "/portfolio/static/media/phone.ab36c3a0.svg"
+  },
+  {
+    "revision": "51466c1cafa19fc837d9267555ec7042",
+    "url": "/portfolio/static/media/pin.51466c1c.svg"
+  },
+  {
+    "revision": "06fd5854a3db83e232bb6ee6dc057068",
+    "url": "/portfolio/static/media/resume.06fd5854.pdf"
+  },
+  {
+    "revision": "49a0262d6cd8957e814022916ee0c798",
+    "url": "/portfolio/static/media/tie.49a0262d.svg"
+  },
+  {
+    "revision": "eec29ab7568b0358620f077b1fa23258",
+    "url": "/portfolio/static/media/youtube.eec29ab7.svg"
+  }
+]);
